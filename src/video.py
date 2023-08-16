13e946ba504469ff709bc2063c77d959ea0c42d5
Katya <basagan13@gmail.com>
2023-08-16 17:43:38 +0300

    fixing video,py

--- src/video.py
+++ src/video.py
@@ -36,9 +36,3 @@ class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-        video_response = self.video_response
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.video_url = f'https://youtu.be/{self.video_id}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
